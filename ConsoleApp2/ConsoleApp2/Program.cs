6fbc4494fae540dab01ec783d0657729a2475592
Pragati <tanpure0011@gmail.com>
2022-05-24 17:52:07 +0530

    first commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int row=0; row<5; row++)
+            {
+                for(int col=5; col<row; col--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for(int row=0; row<5; row++)
+            {
+                for(int col=0; col<row; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+            
+    }
+}
