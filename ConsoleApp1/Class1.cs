d12f927c8980ec0226575aaf9f61a0db9b355be4
Pragati <tanpure0011@gmail.com>
2022-05-24 13:03:50 +0530

    first commit

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter yor string: ");
+			String a = Console.ReadLine();
+
+			char[] temp = a.ToCharArray();
+			Array.Reverse(temp);
+			string b = new string(temp);
+
+			if (a.Equals(b))
+			{
+				Console.WriteLine("The given string is palindrome");
+			}
+			else
+			{
+				Console.WriteLine("The given string is not a palindrome");
+			}
+
+		}
+	}
+}
